89b54e2cfc073b70bbcbd07267b94e791b036bf0
kirito <kirito666codr@gmail.com>
2025-05-30 22:58:02 +0530

    Add express-rate-limit middleware for request rate limiting

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,10 +1,15 @@
 
+import rateLimit from 'express-rate-limit'
 import cors from "cors"
 import morgan from "morgan"
 import express from "express"
 
 const app = express()
 
+app.use(rateLimit({
+    windowMs:1*60*1000,
+    max:100
+}))
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
