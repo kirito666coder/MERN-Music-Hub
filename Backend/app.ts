690c979a6bb999e5aeaf63a6fc7d3aed740542db
kirito <kirito666codr@gmail.com>
2025-05-30 22:52:53 +0530

    Add CORS middleware for handling cross-origin requests

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,9 +1,14 @@
 
+import cors from "cors"
 import morgan from "morgan"
 import express from "express"
 
 const app = express()
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(morgan("dev"))
 
 app.use(express.json())
