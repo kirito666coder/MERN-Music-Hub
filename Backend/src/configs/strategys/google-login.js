b42924bf281d067875b7d6f4999395903a6a1611
kirito <kirito666codr@gmail.com>
2025-06-06 17:14:05 +0530

    feat: implement Google OAuth strategy with environment variables for client ID and secret

--- Backend/src/configs/strategys/google-login.js
+++ Backend/src/configs/strategys/google-login.js
@@ -1,5 +1,13 @@
-
+import GoogleStrategy from 'passport-google-oauth20'
 
 export const google = (passport)=>{
 
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+    const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+    passport.use(new GoogleStrategy({
+         clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:300/api/auth/google/callback"
+    }))
 }
