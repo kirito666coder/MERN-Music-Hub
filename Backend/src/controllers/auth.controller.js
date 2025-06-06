c8ba72cd6a49395df5d23a6f525e6f4c1db35173
kirito <kirito666codr@gmail.com>
2025-06-06 18:11:23 +0530

    feat: add jsonwebtoken dependency and implement token generation in Google callback

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,16 @@
-
-
+import jwt from 'jsonwebtoken'
 
 export const GoogleCallBack = (req,res)=>{
-    
+    try {
+        const user = req.user;
+
+        if(!user){
+            res.status(401).json({message:"Authentication failed"})
+        }
+
+        const token = jwt.sign({_id:user._id},)
+        
+    } catch (error) {
+        res.status(500).json({message:'internal server error during Google Login'})
+    }
 }
