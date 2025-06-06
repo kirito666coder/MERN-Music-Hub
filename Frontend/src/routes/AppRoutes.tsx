dbc1ca03d793a30c256951a8ccd4d6f37a7122b8
kirito <kirito666codr@gmail.com>
2025-06-06 19:46:02 +0530

    feat: add Login component to AppRoutes for login path

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom"
+import Login from "../pages/Login"
 
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path="/login" element={}/>
+        <Route path="/login" element={
+            <Login/>
+        }/>
 
 
     </Routes>
