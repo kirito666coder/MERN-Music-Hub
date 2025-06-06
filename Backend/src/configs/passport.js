62dbad3de6698b271211b05301365f735923eb96
kirito <kirito666codr@gmail.com>
2025-06-06 23:47:54 +0530

    feat: integrate passport-jwt strategy into passport configuration

--- Backend/src/configs/passport.js
+++ Backend/src/configs/passport.js
@@ -1,7 +1,9 @@
 import passport from "passport";
 import { google } from "./strategys/google-login.js";
+import passportJwt from "./strategys/passport-jwt.js";
 
 google(passport)
 
+passportJwt(passport)
 
 export default passport;
