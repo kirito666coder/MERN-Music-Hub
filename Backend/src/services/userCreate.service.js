7ba4d07e5d9431fe495d3a5fddc92c0d0331da4a
kirito <kirito666codr@gmail.com>
2025-06-06 23:07:55 +0530

    refactor: improve createUser function structure and enhance readability

--- Backend/src/services/userCreate.service.js
+++ Backend/src/services/userCreate.service.js
@@ -1,29 +1,37 @@
 import UserModel from "../Models/user.model.js"
 
 
-const createUser = async ({email,providerName,providerId,profile})=>{
-
-    const query = {
-        $or:[
-            {email},
-        ]
-    }
-
-    const update = {
-        $setOnInsert:{
-            name:profile.displayName,
-            email:email,
-            image:profile.photos[0]?.value || null,
-            [providerName]:providerId,
+const createUser = async ({ email, providerName, providerId, profile }) => {
+    try {
+        const query = {
+            $or: [
+                { email },
+                { [providerName]: providerId }
+            ]
         }
+
+        const existingUser = await UserModel.findOne(query)
+
+        if (existingUser) {
+
+            if (!existingUser[providerName]) {
+                existingUser[providerName] = providerId
+                await existingUser.save()
+            }
+            return existingUser
+        }
+
+        const user = await UserModel.create({
+            name: profile.displayName,
+            email: email,
+            image: profile.photos[0]?.value || null,
+            [providerName]: providerId,
+        })
+
+        return user;
+    } catch (error) {
+        console.log('Error creating or fetching user', error)
     }
-
-    const user = await UserModel.findOneAndUpdate(query,update,{
-        upsert:true,
-        new:true,
-    })
-
-    return user;
 }
 
 export default createUser;
