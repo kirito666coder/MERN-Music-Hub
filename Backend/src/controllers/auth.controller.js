407e890e36b025a05f6f9ee45dcb8854a6ecdc25
kirito <kirito666codr@gmail.com>
2025-06-06 16:51:41 +0530

    feat: add Google authentication routes and callback handler

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+
+
+
+export const GoogleCallBack = (req,res)=>{
+    
+}
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,7 +1,17 @@
 
 import { Router } from "express";
+import passport from "passport";
+import { GoogleCallBack } from "../controllers/auth.controller.js";
 
 const AuthRoute = Router()
 
+app.get('/google',
+    passport.authenticate('google',{scope:['profile']})
+)
+
+app.get('/google/callback',
+    passport.authenticate('google',{session:false}),
+    GoogleCallBack
+)
 
 export default AuthRoute;
