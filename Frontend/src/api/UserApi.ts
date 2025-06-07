5e07b566a0d0fe36a88e2663729ed2a3e290e955
kirito <kirito666codr@gmail.com>
2025-06-07 15:31:11 +0530

    feat: enhance GetUserApi function to return User type or null

--- Frontend/src/api/UserApi.ts
+++ Frontend/src/api/UserApi.ts
@@ -1,13 +1,17 @@
 import axios from "axios"
+import type { User } from "../types/user";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE.BACKEND_URL,
-    withCredentials:true,
+    withCredentials: true,
 })
 
-export const GetUserApi = async ()=>{
-      
-    const res = await api.get("/api/auth/profile")
-
-    return res;
+export const GetUserApi = async (): Promise<User | null> => {
+    try {
+        const res = await api.get<User>("/api/auth/profile")
+        return res.data;
+    } catch (error) {
+        console.log("Error fetching user:", error)
+        return null;
+    }
 }
