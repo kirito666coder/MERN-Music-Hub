449ba6a8eee39fce44032f45dac44551a5d8cfd4
kirito <kirito666codr@gmail.com>
2025-06-07 15:17:39 +0530

    feat: implement GetUserApi function for user profile retrieval

---
+++ Frontend/src/api/UserApi.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE.BACKEND_URL
+})
+
+export const GetUserApi = async ()=>{
+      
+    const res = await api.get("/api/auth/profile")
+
+    return res;
+}
