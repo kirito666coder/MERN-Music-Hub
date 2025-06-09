2ccd06e46c47e7919fc4a80fd380f4b8e249545e
kirito <kirito666codr@gmail.com>
2025-06-09 15:09:53 +0530

    feat: fetch user data on app initialization using useEffect

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react"
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./routes/AppRoutes"
+import { useDispatch } from "react-redux"
+import type { AppDispatch } from "./app/store"
+import { fetchUser } from "./features/user/userSlice"
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    dispatch(fetchUser())
+  }, [dispatch])
+  
+
   return (
     <BrowserRouter>
       <AppRoutes/>
