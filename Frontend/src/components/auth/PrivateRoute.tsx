3641b6af1144e1b548b1d10dd6aad8bb88a465a4
kirito <kirito666codr@gmail.com>
2025-06-09 15:25:06 +0530

    feat: implement PrivateRoute component for user authentication and loading state

--- Frontend/src/components/auth/PrivateRoute.tsx
+++ Frontend/src/components/auth/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import type { RootState } from "../../app/store"
 import { Navigate } from "react-router-dom"
 
-const PublicRoutes = ({children}:{children:JSX.Element}) => {
+const PrivateRoute = ({children}:{children:JSX.Element}) => {
   const {user,loading} = useSelector((state:RootState)=>state.user)
 
   if(loading) return <div>Loading...</div>
@@ -11,4 +11,4 @@ const PublicRoutes = ({children}:{children:JSX.Element}) => {
   
 }
 
-export default PublicRoutes
+export default PrivateRoute;
