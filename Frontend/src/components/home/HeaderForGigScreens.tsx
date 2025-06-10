a058dad8254443e4eb13c4484767fed70dbd6939
kirito <kirito666codr@gmail.com>
2025-06-11 00:59:25 +0530

    feat: add Logo component to HeaderForGigScreens

--- Frontend/src/components/home/HeaderForGigScreens.tsx
+++ Frontend/src/components/home/HeaderForGigScreens.tsx
@@ -1,10 +1,11 @@
+import Logo from "./homeComponents/Logo"
 import SearchBar from "./homeComponents/SearchBar"
 
 
 const HeaderForGigScreens = () => {
   return (
     <div className="p-2">
-      
+      <Logo/>
       <SearchBar/>
     </div>
   )
