777698f29f83669094e33456441654c888e4fa1e
kirito <kirito666codr@gmail.com>
2025-06-10 23:29:35 +0530

    feat: update HeaderForGigScreens component to enhance layout and styling

--- Frontend/src/components/home/HeaderForGigScreens.tsx
+++ Frontend/src/components/home/HeaderForGigScreens.tsx
@@ -3,9 +3,11 @@
 const HeaderForGigScreens = () => {
   return (
     <div className="p-2">
-      <h1 className="text-gray-500 text-xl font-bold">
+      <h1 className="text-gray-500 text-xl font-bold flex gap-0.5">
+        <i>
         Hello
-        <span></span>
+        </i>
+        <span className="text-sky-400 font-normal">Music</span>
       </h1>
     </div>
   )
