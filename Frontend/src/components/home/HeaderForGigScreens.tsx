282eca372d62c23a7c19aca7577b9bdbff439b46
kirito <kirito666codr@gmail.com>
2025-06-11 00:58:20 +0530

    feat: add Logo component for branding in the application

--- Frontend/src/components/home/HeaderForGigScreens.tsx
+++ Frontend/src/components/home/HeaderForGigScreens.tsx
@@ -4,12 +4,7 @@ import SearchBar from "./homeComponents/SearchBar"
 const HeaderForGigScreens = () => {
   return (
     <div className="p-2">
-      <h1 className="text-gray-500 text-xl font-bold flex gap-0.5">
-        <i>
-        Hello
-        </i>
-        <span className="text-sky-400 font-normal">Music</span>
-      </h1>
+      
       <SearchBar/>
     </div>
   )
