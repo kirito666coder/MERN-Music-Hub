8536c0b07686fcd6137d7505020aaf92b6e9da45
kirito <kirito666codr@gmail.com>
2025-06-11 00:58:57 +0530

    feat: implement Logo component with branding text and styling

--- Frontend/src/components/home/homeComponents/Logo.tsx
+++ Frontend/src/components/home/homeComponents/Logo.tsx
@@ -1,10 +1,13 @@
 
 const Logo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <h1 className="text-gray-500 text-xl font-bold flex gap-0.5">
+            <i>
+                Hello
+            </i>
+            <span className="text-sky-400 font-normal">Music</span>
+        </h1>
+    )
 }
 
 export default Logo
