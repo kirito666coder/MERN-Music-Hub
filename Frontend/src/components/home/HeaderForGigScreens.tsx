2b0a2edc912de897da5fb775a8b4a9b5b195c618
kirito <kirito666codr@gmail.com>
2025-06-11 01:00:19 +0530

    feat: enhance layout of HeaderForGigScreens with flexbox styling

--- Frontend/src/components/home/HeaderForGigScreens.tsx
+++ Frontend/src/components/home/HeaderForGigScreens.tsx
@@ -4,7 +4,7 @@ import SearchBar from "./homeComponents/SearchBar"
 
 const HeaderForGigScreens = () => {
   return (
-    <div className="p-2">
+    <div className="p-2 flex justify-around items-center gap-2">
       <Logo/>
       <SearchBar/>
     </div>
