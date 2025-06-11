f264c6a3c474504cc5a5b9f4a6b0db9f73389eb9
kirito <kirito666codr@gmail.com>
2025-06-11 22:39:02 +0530

    feat: add NotificationLogo component to HeaderForGigScreens

--- Frontend/src/components/home/HeaderForGigScreens.tsx
+++ Frontend/src/components/home/HeaderForGigScreens.tsx
@@ -1,4 +1,5 @@
 import Logo from "./homeComponents/Logo"
+import NotificationLogo from "./homeComponents/NotificationLogo"
 import SearchBar from "./homeComponents/SearchBar"
 
 
@@ -7,6 +8,9 @@ const HeaderForGigScreens = () => {
     <div className="p-2 flex justify-around items-center gap-2">
       <Logo/>
       <SearchBar/>
+      <div>
+        <NotificationLogo/>
+      </div>
     </div>
   )
 }
