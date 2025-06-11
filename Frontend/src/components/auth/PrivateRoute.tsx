48db9609a23d9ad9f23b19e3ffd9631c9452b16f
kirito <kirito666codr@gmail.com>
2025-06-11 23:19:40 +0530

    feat: remove console log statements from PrivateRoute component for cleaner code

--- Frontend/src/components/auth/PrivateRoute.tsx
+++ Frontend/src/components/auth/PrivateRoute.tsx
@@ -4,9 +4,8 @@ import { Navigate } from "react-router-dom"
 
 const PrivateRoute = ({children}:{children:JSX.Element}) => {
   const {user,loading} = useSelector((state:RootState)=>state.user)
-   console.log("user",user)
+
    if(loading) return <div>Loading...</div>
-   console.log("user",user)
 
   return user ? children : <Navigate to="/login"/>
   
