4a690e784e4e9381c413cd189e833398aea7e446
kirito <kirito666codr@gmail.com>
2025-06-12 00:20:06 +0530

    feat: add NavBarForSmallScreens component to NavBar for responsive design

--- Frontend/src/components/NavBar/NavBar.tsx
+++ Frontend/src/components/NavBar/NavBar.tsx
@@ -1,8 +1,11 @@
+import NavBarForBigScreens from "./NavBarForBigScreens"
+import NavBarForSmallScreens from "./NavBarForSmallScreens"
 
 const NavBar = () => {
   return (
     <nav>
-      
+      <NavBarForBigScreens/>
+      <NavBarForSmallScreens/>
     </nav>
   )
 }
