fb56925395d3a0d17065fd21434a63b00e617bbb
kirito <kirito666codr@gmail.com>
2025-06-12 00:17:08 +0530

    feat: implement Header component in MainLayout for improved layout structure

---
+++ Frontend/src/components/Headers/Header.tsx
@@ -0,0 +1,13 @@
+import HeaderForBigScreens from "./HeaderForBigScreens"
+import HeaderForSmallScreens from "./HeaderForSmallScreens"
+
+const Header = () => {
+  return (
+    <header>
+        <HeaderForBigScreens/>
+        <HeaderForSmallScreens/>
+      </header>
+  )
+}
+
+export default Header
--- Frontend/src/components/MainLayout.tsx
+++ Frontend/src/components/MainLayout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom"
-import HeaderForBigScreens from "./Headers/HeaderForBigScreens"
-import HeaderForSmallScreens from "./Headers/HeaderForSmallScreens"
 
 const MainLayout = () => {
   return (
     <>
-      <header>
-        <HeaderForBigScreens/>
-        <HeaderForSmallScreens/>
-      </header>
+      
       <nav>
 
       </nav>
