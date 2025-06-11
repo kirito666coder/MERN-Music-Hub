2dc9a1c47b097b3562d9556c20c589b8318f3342
kirito <kirito666codr@gmail.com>
2025-06-12 00:00:31 +0530

    feat: remove unused header imports from Home component

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,6 +1,3 @@
-import HeaderForBigScreens from "../components/Headers/HeaderForBigScreens"
-import HeaderForSmallScreens from "../components/Headers/HeaderForSmallScreens"
-
 
 const Home = () => {
   return (
