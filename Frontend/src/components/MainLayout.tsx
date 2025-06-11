d7ba66c6c639428f1adb3fd0522744b250d1eff5
kirito <kirito666codr@gmail.com>
2025-06-12 00:20:19 +0530

    feat: add NavBar component to MainLayout for improved navigation

--- Frontend/src/components/MainLayout.tsx
+++ Frontend/src/components/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Headers/Header"
+import NavBar from "./NavBar/NavBar"
 
 const MainLayout = () => {
   return (
     <>
       <Header/>
-      
+      <NavBar/>
       <Outlet/>
     </>
   )
