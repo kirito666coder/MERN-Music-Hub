fd9b7135a6306ac41e91f2a71cbd72634c221d99
kirito <kirito666codr@gmail.com>
2025-06-12 00:17:33 +0530

    feat: add Header component to MainLayout for improved layout structure

--- Frontend/src/components/MainLayout.tsx
+++ Frontend/src/components/MainLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Header from "./Headers/Header"
 
 const MainLayout = () => {
   return (
     <>
-      
+      <Header/>
       <nav>
 
       </nav>
