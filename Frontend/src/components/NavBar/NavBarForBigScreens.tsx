7f98340decbbd6d94758eac4fb49acfa9fcac36a
kirito <kirito666codr@gmail.com>
2025-06-13 00:01:47 +0530

    fix: implement NavBarForBigScreens component with navigation links

--- Frontend/src/components/NavBar/NavBarForBigScreens.tsx
+++ Frontend/src/components/NavBar/NavBarForBigScreens.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
 
 const NavBarForBigScreens = () => {
   return (
     <div className="bg-red-400 h-[100%]">
-      ss
+      
+      <ul>
+        <Link to={'/'}><li>home</li></Link>
+        <Link to={'/'}><li>Genre</li></Link>
+        <Link to={'/'}><li>my Libary</li></Link>
+        <Link to={'/'}><li>lied songs</li></Link>
+      </ul>
+
     </div>
   )
 }
