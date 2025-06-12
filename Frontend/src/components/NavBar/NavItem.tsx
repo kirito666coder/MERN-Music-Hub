912413d3ad45079e01fcd4df861295c71703d97a
kirito <kirito666codr@gmail.com>
2025-06-13 01:10:22 +0530

    fix: update NavItem component to use active state for icon rendering

--- Frontend/src/components/NavBar/NavItem.tsx
+++ Frontend/src/components/NavBar/NavItem.tsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 interface NavItemProps{
     to:string,
     label:string,
-    icon:React.ReactNode
+    icon:(isActive:boolean)=>React.ReactNode
 }
 
 const NavItem = ({to,label,icon}:NavItemProps) => {
+      const location = useLocation()
+  const isActive = location.pathname === to
   return (
     <Link to={to} className="block">
     <li className=" px-4 py-2 rounded-md hover:opacity-70 flex flex-col md:flex-row justify-center items-center">  
-    <span>{icon}</span>
+    <span>{icon(isActive)}</span>
     <span className=" font-medium">{label}</span>
     </li>
     </Link>
