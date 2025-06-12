85c67e3e8782588450fc70bde2a7a6c61ec78914
kirito <kirito666codr@gmail.com>
2025-06-13 00:20:35 +0530

    feat: add icon prop to NavItem component for enhanced UI

--- Frontend/src/components/NavBar/NavItem.tsx
+++ Frontend/src/components/NavBar/NavItem.tsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom"
 
 interface NavItemProps{
     to:string,
-    label:string
+    label:string,
+    icon:React.ReactNode
 }
 
-const NavItem = ({to,label}:NavItemProps) => {
+const NavItem = ({to,label,icon}:NavItemProps) => {
   return (
     <Link to={to} className="block">
-    <li className="px-4 py-2 hover:bg-red-500 rounded-md">{label}</li>
+    <li className="px-4 py-2 hover:bg-red-500 rounded-md">
+    {icon}
+    {label}</li>
     </Link>
   )
 }
