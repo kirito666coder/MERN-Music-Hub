67977404e3627c9d99898c86caeaf73b5c97cda0
kirito <kirito666codr@gmail.com>
2025-06-13 00:10:40 +0530

    fix: remove unnecessary div wrapper in SongIconSvg component

--- Frontend/src/icons/SongIconSvg.tsx
+++ Frontend/src/icons/SongIconSvg.tsx
@@ -1,9 +1,18 @@
 
 const SongIconSvg = () => {
   return (
-    <div>
-      
-    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+  stroke="currentColor" className="w-12 h-12">
+  <defs>
+    <linearGradient id="musicGradient" x1="0" y1="0" x2="1" y2="1">
+      <stop offset="0%" stop-color="#d1d5db" /> 
+      <stop offset="100%" stop-color="#3b82f6" /> 
+    </linearGradient>
+  </defs>
+  <path stroke="url(#musicGradient)" stroke-linecap="round" stroke-linejoin="round"
+    d="M9 18.75a3.75 3.75 0 1 1-3.75-3.75M9 18.75V6.562a.75.75 0 0 1 .577-.73l9-2.25a.75.75 0 0 1 .946.73v10.94a3.75 3.75 0 1 1-1.5-2.988V5.35L9 7.712v11.038z" />
+</svg>
+
   )
 }
 
