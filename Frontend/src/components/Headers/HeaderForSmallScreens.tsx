2a514ec973c822d5e7566cc001edc8a7cdbebc4d
kirito <kirito666codr@gmail.com>
2025-06-13 19:32:14 +0530

    feat: replace Profile component with ProFileDropDownMenuForSmallScreens in HeaderForSmallScreens

--- Frontend/src/components/Headers/HeaderForSmallScreens.tsx
+++ Frontend/src/components/Headers/HeaderForSmallScreens.tsx
@@ -1,13 +1,13 @@
 import Logo from "../MainLayoutComponents/Logo"
 import NotificationLogo from "../MainLayoutComponents/NotificationLogo"
-import Profile from "../MainLayoutComponents/Profile"
+import ProFileDropDownMenuForSmallScreens from "./ProFileDropDownMenuForSmallScreens"
 
 const HeaderForSmallScreens = () => {
   return (
     <div className="p-2 md:hidden flex justify-between px-5 items-center gap-2 max-w-screen">
       <NotificationLogo/>
       <Logo/>
-      <Profile/>
+      <ProFileDropDownMenuForSmallScreens/>
     </div>
   )
 }
