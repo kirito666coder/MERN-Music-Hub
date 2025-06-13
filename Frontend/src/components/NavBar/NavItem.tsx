f58dfd54494d594dbee3987decf4740146839fe0
kirito <kirito666codr@gmail.com>
2025-06-13 19:04:56 +0530

    fix: adjust NavItem structure for improved accessibility and styling consistency

--- Frontend/src/components/NavBar/NavItem.tsx
+++ Frontend/src/components/NavBar/NavItem.tsx
@@ -13,12 +13,12 @@ const NavItem = ({ to, label, icon }: NavItemProps) => {
     console.log(location.pathname ,to,isActive)
 
     return (
-        <Link to={to} className="block">
-            <li className={` px-4 py-2 rounded-md ${isActive?'md:bg-gradient-to-r bg-gradient-to-t from-rose-600/20 via-rose-600/5 to-pink-100/5':'opacity-80'} transition-colors hover:opacity-70 flex flex-col md:flex-row justify-center items-center `}>
+        <li className="flex-1">
+            <Link to={to} className={` px-4 py-2 ${isActive?'md:bg-gradient-to-r bg-gradient-to-t from-rose-600/20 via-rose-600/5 to-pink-100/5':'opacity-80'} transition-colors hover:opacity-70 flex flex-col md:flex-row justify-center items-center md:h-15 md:w-28 md:rounded-s-md`}>
                 <span>{icon(isActive)}</span>
-                <span className={`${isActive ? "text-rose-600" : ""} font-medium`}>{label}</span>
-            </li>
-        </Link>
+                <span className={`${isActive ? "text-rose-500" : ""} font-medium`}>{label}</span>
+            </Link>
+        </li>
     )
 }
 
