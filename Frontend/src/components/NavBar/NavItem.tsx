dfd6ff4056779a0445053bc5ff538fb283b76893
kirito <kirito666codr@gmail.com>
2025-06-13 16:35:02 +0530

    fix: update NavItem component gradient styling for active state

--- Frontend/src/components/NavBar/NavItem.tsx
+++ Frontend/src/components/NavBar/NavItem.tsx
@@ -14,7 +14,7 @@ const NavItem = ({ to, label, icon }: NavItemProps) => {
 
     return (
         <Link to={to} className="block">
-            <li className={` px-4 py-2 rounded-md ${isActive?'md:bg-gradient-to-r bg-gradient-to-t from-rose-600/20 via-pink-100/5 to-pink-100/5':''} transition-colors hover:opacity-70 flex flex-col md:flex-row justify-center items-center `}>
+            <li className={` px-4 py-2 rounded-md ${isActive?'md:bg-gradient-to-r bg-gradient-to-t from-rose-600/20 via-rose-600/5 to-pink-100/5':'opacity-80'} transition-colors hover:opacity-70 flex flex-col md:flex-row justify-center items-center `}>
                 <span>{icon(isActive)}</span>
                 <span className={`${isActive ? "text-rose-600" : ""} font-medium`}>{label}</span>
             </li>
