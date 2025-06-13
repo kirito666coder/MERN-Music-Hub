96bcc094b298c56c29042b2540bb6b3214c875a7
kirito <kirito666codr@gmail.com>
2025-06-14 01:01:04 +0530

    feat: refactor TrandingNewHits component by removing the Like button and adjusting layout

--- Frontend/src/components/home/TrandingNewHits.tsx
+++ Frontend/src/components/home/TrandingNewHits.tsx
@@ -1,3 +1,4 @@
+import LikeIconSvg from "@/icons/LikeIconSvg"
 
 const TrandingNewHits = () => {
     return (
@@ -6,15 +7,13 @@ const TrandingNewHits = () => {
                 <h2 className=" text-xs pt-3 opacity-70">Tranding New Hit</h2>
                 <h1 className="text-4xl font-bold pt-3">Lovely</h1>
                 <h3 className="font-semibold text-sm pt-2">Billie Eilish</h3>
-
+                  
+                  <div className="flex items-center gap-4">
                 <button className="px-4 py-0.5 rounded-2xl border-2 border-white mt-2 text-md bg-gradient-to-br from-[#f43f5e] to-[#3b82f6] hover:brightness-110 transition-all duration-300 font-semibold ">
                     Listen Now
                 </button>
-
-
-                <button>
-                    Like
-                </button>
+                <LikeIconSvg/>
+                  </div>
             </div>
 
         </div>
