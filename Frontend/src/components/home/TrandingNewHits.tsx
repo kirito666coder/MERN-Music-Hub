11468c040470a7c86da01a9cfe0373188f92b794
kirito <kirito666codr@gmail.com>
2025-06-14 00:44:37 +0530

    feat: add content structure to TrandingNewHits component

--- Frontend/src/components/home/TrandingNewHits.tsx
+++ Frontend/src/components/home/TrandingNewHits.tsx
@@ -2,7 +2,18 @@
 const TrandingNewHits = () => {
   return (
     <div className="bg-gradient-to-br from-cyan-800/90  via-cyan-950 to-cyan-950 mx-2.5 mt-4 rounded-md h-40">
-        
+    <h2>Tranding New Hit</h2>
+    <h1>Lovely</h1>
+    <h3>Billie Eilish</h3>
+
+    <button>
+        listen Now
+    </button>
+
+    <button>
+        Like
+    </button>
+
 </div>
 
   )
