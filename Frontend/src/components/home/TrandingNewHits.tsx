b03e208032106d71c076f4eabe79ed4ddb81f969
kirito <kirito666codr@gmail.com>
2025-06-14 00:42:24 +0530

    feat: enhance TrandingNewHits component with background styling

--- Frontend/src/components/home/TrandingNewHits.tsx
+++ Frontend/src/components/home/TrandingNewHits.tsx
@@ -1,9 +1,10 @@
 
 const TrandingNewHits = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="bg-gradient-to-br from-cyan-800/90  via-cyan-950 to-cyan-950 mx-2.5 mt-4 rounded-md h-40">
+        
+</div>
+
   )
 }
 
