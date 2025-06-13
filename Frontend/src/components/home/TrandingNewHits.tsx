5a0049453826c8b34da25a111906488f50f7d311
kirito <kirito666codr@gmail.com>
2025-06-14 00:50:00 +0530

    feat: improve layout of TrandingNewHits component with additional styling

--- Frontend/src/components/home/TrandingNewHits.tsx
+++ Frontend/src/components/home/TrandingNewHits.tsx
@@ -2,9 +2,10 @@
 const TrandingNewHits = () => {
   return (
     <div className="bg-gradient-to-br from-cyan-800/90  via-cyan-950 to-cyan-950 mx-2.5 mt-4 rounded-md h-40">
-    <h2>Tranding New Hit</h2>
-    <h1>Lovely</h1>
-    <h3>Billie Eilish</h3>
+        <div className="px-8">
+    <h2 className=" text-xs pt-3 opacity-70">Tranding New Hit</h2>
+    <h1 className="text-4xl font-bold pt-3">Lovely</h1>
+    <h3 className="font-semibold text-sm pt-2">Billie Eilish</h3>
 
     <button>
         listen Now
@@ -13,6 +14,7 @@ const TrandingNewHits = () => {
     <button>
         Like
     </button>
+        </div>
 
 </div>
 
