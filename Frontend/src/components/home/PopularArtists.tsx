1c978da78a7067ace21cd68afde5da90dbb5e69c
kirito <kirito666codr@gmail.com>
2025-06-14 19:04:49 +0530

    add PopularArtistsCard on PopularArtistsCard component

--- Frontend/src/components/home/PopularArtists.tsx
+++ Frontend/src/components/home/PopularArtists.tsx
@@ -1,8 +1,9 @@
+import PopularArtistsCard from "./Components/PopularArtistsCard"
 
 const PopularArtists = () => {
   return (
     <div className="bg-white h-70 m-2.5">
-      
+      <PopularArtistsCard/>
     </div>
   )
 }
