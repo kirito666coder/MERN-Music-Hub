e6a9292accb90018c1bc8121db757bd8aab23f35
kirito <kirito666codr@gmail.com>
2025-06-15 01:15:13 +0530

    style: enhance layout of PopularArtistsCard with updated dimensions and artist display

--- Frontend/src/components/home/Components/PopularArtistsCard.tsx
+++ Frontend/src/components/home/Components/PopularArtistsCard.tsx
@@ -3,9 +3,16 @@ const PopularArtistsCard = () => {
   return (
     <div className=" relative">
       <img src="./assets/image.jpg" alt="" className=" h-90 min-w-115 rounded-2xl " />
-      <div className=" flex justify-center items-center h-15 w-full absolute bottom-0 left-0">
-        <div className=" w-[90%] h-[95%] rounded-full bg-accent/40 border-2 dark:border-white/30 border-black/30 ">
+      <div className=" flex justify-center items-center h-19 w-full absolute bottom-0 left-0">
+        <div className=" w-[90%] h-[95%] rounded-full bg-accent/40 border-2 dark:border-white/30 border-black/30 flex justify-between items-center">
+          <div className="flex  gap-2.5 items-center h-full border-2 border-blue-700">
+        <img src="./assets/image.jpg" alt="" className=" w-15 h-15 rounded-full object-cover " />
+            <h3 className="text-2xl font-semibold">Billie Eilish</h3>
+            
+          </div>
+          <div>
 
+          </div>
         </div>
       </div>
     </div>
