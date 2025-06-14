23e817fab756a8fe659d7dad27be7e6235f75d3c
kirito <kirito666codr@gmail.com>
2025-06-14 18:42:15 +0530

    feat: add PopularSongs component to Home page

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,9 +1,11 @@
+import PopularSongs from "@/components/home/PopularSongs"
 import TrandingNewHits from "@/components/home/TrandingNewHits"
 
 const Home = () => {
   return (
     <>
       <TrandingNewHits/>
+      <PopularSongs/>
     </>
   )
 }
