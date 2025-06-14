84ec3d02befb89c209447257020f1c5e89ca463b
kirito <kirito666codr@gmail.com>
2025-06-14 18:36:06 +0530

    fix: add relative positioning to TrandingNewHits component for improved layout

--- Frontend/src/components/home/TrandingNewHits.tsx
+++ Frontend/src/components/home/TrandingNewHits.tsx
@@ -2,7 +2,7 @@ import LikeButton from "../icons/LikeButton"
 
 const TrandingNewHits = () => {
     return (
-        <div className="bg-gradient-to-br from-cyan-800/90  via-cyan-950 to-cyan-950 mx-2.5 mt-4 rounded-md h-40 flex justify-between overflow-hidden">
+        <div className="bg-gradient-to-br from-cyan-800/90  via-cyan-950 to-cyan-950 mx-2.5 mt-4 rounded-md h-40 flex justify-between overflow-hidden relative">
             <div className="px-8 text-white w-full">
                 <h2 className=" text-xs pt-3 opacity-70">Tranding New Hit</h2>
                 <h1 className="text-4xl font-bold pt-3">Lovely</h1>
@@ -16,7 +16,9 @@ const TrandingNewHits = () => {
                   </div>
             </div>
 
-            <div className=" w-full flex  justify-end gap-1">
+                 <h4 className="text-lg font-semibold opacity-90 absolute bottom-5 left-[48%] " >6Million Plays</h4>
+
+            <div className=" w-full flex  justify-end gap-1 ">
                  <img src="./assets/image.jpg" alt="image" className="w-39 h-full object-cover grayscale hover:grayscale-0 transition-all duration-300" />
                  <img src="./assets/image1.jpg" alt="image" className="w-30 h-full object-cover hidden md:block grayscale hover:grayscale-0 transition-all duration-300" />
                  <img src="./assets/image2.jpg" alt="image" className="w-50 h-full object-cover hidden md:block grayscale hover:grayscale-0 transition-all duration-300" />
