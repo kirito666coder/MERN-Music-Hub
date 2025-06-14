32609256068442a9a1d0b978822d3b09894d2376
kirito <kirito666codr@gmail.com>
2025-06-15 01:37:44 +0530

    style: wrap PopularArtists content in a div for improved layout consistency

--- Frontend/src/components/home/PopularArtists.tsx
+++ Frontend/src/components/home/PopularArtists.tsx
@@ -2,10 +2,13 @@ import PopularArtistsCard from "./Components/PopularArtistsCard"
 
 const PopularArtists = () => {
   return (
+    <div>
+
     <div className="mt-10 h-90 m-2.5 flex gap-5 md:gap-0 justify-around overflow-x-scroll overflow-y-hidden md:w-[99%] w-[96%] md:overflow-hidden scroll-smooth  [scrollbar-width:0] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden ">
       <PopularArtistsCard/>
       <PopularArtistsCard/>
       <PopularArtistsCard/>
+    </div>
     </div>
   )
 }
