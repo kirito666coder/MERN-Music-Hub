e802ca51dafe73dd7e8ba8486b145069c9b4007f
kirito <kirito666codr@gmail.com>
2025-06-15 01:27:23 +0530

    feat: implement PlayButtonForPopularArtistsCard with interactive button

--- Frontend/src/components/home/Components/PlayButtonForPopulareARtistsCardAndItFroPopularArtists.tsx
+++ Frontend/src/components/home/Components/PlayButtonForPopulareARtistsCardAndItFroPopularArtists.tsx
@@ -1,10 +1,10 @@
 
 const PlayButtonForPopulareARtistsCardAndItFroPopularArtists = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <button className="px-4 py-0.5 rounded-2xl border-2 border-white mt-2 text-md bg-gradient-to-br from-[#f43f5e] to-[#3b82f6] hover:brightness-110 transition-all duration-300 font-semibold ">
+         play
+        </button>
+    )
 }
 
 export default PlayButtonForPopulareARtistsCardAndItFroPopularArtists
