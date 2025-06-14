9e9773a542bf271b0125ecafbc955b2517f04ead
kirito <kirito666codr@gmail.com>
2025-06-15 01:07:11 +0530

    update PopularArtists and PopularArtistsCard components to enhance layout and styling

--- Frontend/src/components/home/Components/PopularArtistsCard.tsx
+++ Frontend/src/components/home/Components/PopularArtistsCard.tsx
@@ -1,10 +1,14 @@
 
 const PopularArtistsCard = () => {
   return (
-    <>
-      <img src="./assets/image.jpg" alt="" className=" h-80 min-w-115 rounded-2xl " />
+    <div className=" relative">
+      <img src="./assets/image.jpg" alt="" className=" h-90 min-w-115 rounded-2xl " />
+      <div className=" flex justify-center items-center h-15 w-full absolute bottom-0 left-0">
+        <div className=" w-[90%] h-[95%] rounded-full bg-accent/40 border-2 dark:border-white/30 border-black/30 ">
 
-    </>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/components/home/PopularArtists.tsx
+++ Frontend/src/components/home/PopularArtists.tsx
@@ -2,7 +2,7 @@ import PopularArtistsCard from "./Components/PopularArtistsCard"
 
 const PopularArtists = () => {
   return (
-    <div className="mt-10 h-80 m-2.5 flex gap-5 md:gap-0 justify-around overflow-x-scroll overflow-y-hidden md:w-[99%] w-[96%] md:overflow-hidden scroll-smooth  [scrollbar-width:0] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden ">
+    <div className="mt-10 h-90 m-2.5 flex gap-5 md:gap-0 justify-around overflow-x-scroll overflow-y-hidden md:w-[99%] w-[96%] md:overflow-hidden scroll-smooth  [scrollbar-width:0] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden ">
       <PopularArtistsCard/>
       <PopularArtistsCard/>
       <PopularArtistsCard/>
