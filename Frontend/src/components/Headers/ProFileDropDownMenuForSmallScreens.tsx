51a4f3e2135f48270b0c87ef1b876ba7f3c873e8
kirito <kirito666codr@gmail.com>
2025-06-15 20:06:15 +0530

    style: enhance ProFileDropDownMenuForSmallScreens with improved user greeting and layout adjustments

--- Frontend/src/components/Headers/ProFileDropDownMenuForSmallScreens.tsx
+++ Frontend/src/components/Headers/ProFileDropDownMenuForSmallScreens.tsx
@@ -12,26 +12,30 @@ import { useSelector } from "react-redux"
 import type { RootState } from "@/app/store"
 
 const ProFileDropDownMenuForSmallScreens = () => {
-    const {user} = useSelector((state:RootState)=>state.user)
-    const {setTheme} = useTheme()
+    const { user } = useSelector((state: RootState) => state.user)
+    const { setTheme } = useTheme()
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger><Profile/></DropdownMenuTrigger>
+            <DropdownMenuTrigger><Profile /></DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuLabel>Hi {user?.name}</DropdownMenuLabel>
+                <DropdownMenuLabel className="flex items-center gap-1">
+                    <h1 className="text-lg font-bold bg-gradient-to-br from-[#f43f5e] to-[#3b82f6] bg-clip-text text-transparent hover:brightness-110 transition-all duration-300">
+                        hi
+                    </h1>
+                {user?.name}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>Profile</DropdownMenuItem>
                 <DropdownMenuItem>Settings</DropdownMenuItem>
                 <DropdownMenuLabel className="border-b-1 pt-4 text-md">Theme</DropdownMenuLabel>
-                <DropdownMenuItem 
-                onClick={()=>{
-                    setTheme("light")
-                }}
+                <DropdownMenuItem
+                    onClick={() => {
+                        setTheme("light")
+                    }}
                 >Light</DropdownMenuItem>
-                <DropdownMenuItem 
-                onClick={()=>{
-                    setTheme("dark")
-                }}
+                <DropdownMenuItem
+                    onClick={() => {
+                        setTheme("dark")
+                    }}
                 >Dark</DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
