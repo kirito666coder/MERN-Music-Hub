b60d1ff4aa33f6bbe85d1c29f0b289bf2df04fa4
kirito <kirito666codr@gmail.com>
2025-06-16 00:29:38 +0530

    style: update LikeList and LikeSongsCard components with enhanced layout and styling for improved visual presentation

--- Frontend/src/components/LikePage/LikeList.tsx
+++ Frontend/src/components/LikePage/LikeList.tsx
@@ -2,7 +2,9 @@ import LikeSongsCard from "./LikeSongsCard"
 
 const LikeList = () => {
   return (
-    <div>
+    <div className=" grid grid-cols-1 md:grid-cols-3 gap-2 mt-6 mx-4">
+      <LikeSongsCard/>
+      <LikeSongsCard/>
       <LikeSongsCard/>
     </div>
   )
--- Frontend/src/components/LikePage/LikeSongsCard.tsx
+++ Frontend/src/components/LikePage/LikeSongsCard.tsx
@@ -1,7 +1,7 @@
 
 const LikeSongsCard = () => {
   return (
-    <div>
+    <div className="bg-gradient-to-br from-[#ff788f] to-[#70a4f7] h-20 w-full rounded-md">
       
     </div>
   )
