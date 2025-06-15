ba32bd0ecedfe3d749639ce76c42e42932970c2d
kirito <kirito666codr@gmail.com>
2025-06-16 00:18:43 +0530

    feat: integrate LikeList component into LikedSong page for displaying liked songs

--- Frontend/src/pages/LikedSong.tsx
+++ Frontend/src/pages/LikedSong.tsx
@@ -1,9 +1,13 @@
+import LikeList from "@/components/LikePage/LikeList"
 
 const LikedSong = () => {
   return (
     <div>
       <div className="flex justify-center items-center text-2xl font-bold">
       <h2>Liked songs</h2>
+      </div>
+      <div className="">
+       <LikeList/>
       </div>
     </div>
   )
