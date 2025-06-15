0cbdc8fee1c363738b463868d6e7996d954072b6
kirito <kirito666codr@gmail.com>
2025-06-15 23:10:05 +0530

    style: update PopularSongsCarForPopularSongsSection to include PlayButton and improve layout for better user interaction

--- Frontend/src/components/home/Components/PopularSongsCarForPopularSongsSection.tsx
+++ Frontend/src/components/home/Components/PopularSongsCarForPopularSongsSection.tsx
@@ -1,11 +1,12 @@
+import PlayButton from "@/components/Buttons/PlayButton"
 
 const PopularSongsCarForPopularSongsSection = () => {
-  return (
-    <div className=" relative">
-      <img src="./assets/image.jpg" alt="" className=" h-40 w-50 object-cover rounded-sm hover:rounded-2xl hover:scale-105 transition-all duration-300" />
+  return ( 
+    <div className=" relative  h-40 w-50 overflow-hidden group hover:rounded-2xl transition-all duration-300">
+      <img src="./assets/image.jpg" alt="" className=" object-cover rounded-sm  h-40 w-50" />
      
-      <button>
-        
+      <button className=" absolute group-hover:bottom-2.5 right-2 bottom-[-100px] transition-all duration-300">
+        <PlayButton/>
       </button>
 
     </div>
--- Frontend/src/components/home/PopularSongs.tsx
+++ Frontend/src/components/home/PopularSongs.tsx
@@ -2,16 +2,15 @@ import PopularSongsCarForPopularSongsSection from "./Components/PopularSongsCarF
 
 const PopularSongs = () => {
   return (
-    <div className="h-40  mt-5">
+    <div className="h-70  mt-5">
      <div className="flex justify-between items-center mx-5 my-2">
       <h3 className="text-2xl font-bold">Popular Songs</h3>
       <span className="font-semibold opacity-55 cursor-pointer">See All</span>
       </div>
       
-      <div className="mx-2.5">
+      <div className="mx-2.5 h-50 w-full overflow-x-scroll flex flex-row gap-3">
         <PopularSongsCarForPopularSongsSection/>
       </div>
-
     </div>
   )
 }
