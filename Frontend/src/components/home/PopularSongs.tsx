cf97d41c02f172d796e41ed270e1794dfa877238
kirito <kirito666codr@gmail.com>
2025-06-15 22:23:55 +0530

    style: enhance PopularSongs component with improved header layout and spacing

--- Frontend/src/components/home/PopularSongs.tsx
+++ Frontend/src/components/home/PopularSongs.tsx
@@ -1,8 +1,11 @@
 
 const PopularSongs = () => {
   return (
-    <div className="h-40">
-      
+    <div className="h-40  mt-5">
+     <div className="flex justify-between items-center mx-5 my-2">
+      <h3 className="text-2xl font-bold">Popular Songs</h3>
+      <span className="font-semibold opacity-55 cursor-pointer">See All</span>
+      </div>
     </div>
   )
 }
