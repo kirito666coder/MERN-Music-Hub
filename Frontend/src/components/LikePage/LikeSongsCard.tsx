e3de13f7648f6326c1659c3e3373850007a13bba
kirito <kirito666codr@gmail.com>
2025-06-16 00:35:06 +0530

    style: update LikeSongsCard component with dark mode gradient for improved visual consistency

--- Frontend/src/components/LikePage/LikeSongsCard.tsx
+++ Frontend/src/components/LikePage/LikeSongsCard.tsx
@@ -1,7 +1,7 @@
 
 const LikeSongsCard = () => {
   return (
-    <div className="bg-gradient-to-br from-[#ff788f] to-[#70a4f7] h-20 w-full rounded-md">
+    <div className="bg-gradient-to-br  from-[#ff788f] to-[#70a4f7] dark:from-[#c4213c] dark:to-[#1770ff] h-20 w-full rounded-md">
       
     </div>
   )
