ada4fdf23b2c8a05bf27f0a121df8a1acf53ce2b
kirito <kirito666codr@gmail.com>
2025-06-16 23:26:48 +0530

    style: enhance LikeSongsCard component layout with song details and image for improved user experience

--- Frontend/src/components/LikePage/LikeSongsCard.tsx
+++ Frontend/src/components/LikePage/LikeSongsCard.tsx
@@ -1,7 +1,20 @@
 
 const LikeSongsCard = () => {
   return (
-    <div className="bg-gradient-to-br  from-[#ff788f] to-[#70a4f7] dark:from-[#c4213c] dark:to-[#1770ff] h-20 w-full rounded-md">
+    <div className="bg-gradient-to-br  from-[#ff788f] to-[#70a4f7] dark:from-[#c4213c] dark:to-[#1770ff] h-20 w-full rounded-md flex items-center">
+      <div className=" flex justify-center items-center gap-2">
+      <img src="./assets/image.jpg" alt="" className="h-15 w-15 rounded-full " />
+      <div className="">
+        <h3 className="text-xl font-bold ">Song name</h3>
+        <div className="flex gap-2  font-semibold ">
+          <span>artistname</span>
+          <span>plays 9M</span>
+          <span>Likes 7M</span>
+        </div>
+
+      </div>
+
+      </div>
       
     </div>
   )
