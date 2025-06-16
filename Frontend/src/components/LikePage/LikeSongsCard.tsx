61809a6fcff52ca06c71d78407e49b904f6dbbd7
kirito <kirito666codr@gmail.com>
2025-06-16 23:39:24 +0530

    feat: add PlayButton to LikeSongsCard for enhanced interactivity and user engagement

--- Frontend/src/components/LikePage/LikeSongsCard.tsx
+++ Frontend/src/components/LikePage/LikeSongsCard.tsx
@@ -1,11 +1,12 @@
+import PlayButton from "../Buttons/PlayButton"
 import LikeButton from "../icons/LikeButton"
 
 const LikeSongsCard = () => {
 
   
   return (
-    <div className="bg-gradient-to-br  from-[#ff788f] to-[#70a4f7] dark:from-[#c4213c] dark:to-[#1770ff] h-20 w-full rounded-md flex items-center">
-      <div className=" flex justify-center items-center gap-2">
+    <div className="bg-gradient-to-br  from-[#ff788f] to-[#70a4f7] dark:from-[#c4213c] dark:to-[#1770ff] h-20 w-full rounded-md flex items-center relative group overflow-hidden">
+      <div className=" ml-2 flex justify-between items-center gap-2  ">
       <img src="./assets/image.jpg" alt="" className="h-15 w-15 rounded-full " />
       <div className="">
         <h3 className="text-xl font-bold ">Song name</h3>
@@ -16,7 +17,9 @@ const LikeSongsCard = () => {
         </div>
 
       </div>
-
+      <button className=" absolute group-hover:bottom-2.5 right-2 bottom-[-100px] transition-all duration-300 ">
+        <PlayButton/>
+      </button>
       </div>
 
     </div>
