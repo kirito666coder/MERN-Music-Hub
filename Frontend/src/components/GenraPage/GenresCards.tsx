9de76c5c9e62994ee81e3bb7a7a17a51121233dd
kirito <kirito666codr@gmail.com>
2025-06-18 01:36:09 +0530

    style: enhance GenresCards component with cursor pointer for improved user interaction

--- Frontend/src/components/GenraPage/GenresCards.tsx
+++ Frontend/src/components/GenraPage/GenresCards.tsx
@@ -1,7 +1,7 @@
 
 const GenresCards = () => {
   return (
-    <div className=" flex flex-col items-center rounded-2xl h-64 md:h-65 w-[99%]  bg-gradient-to-tr from-[#131313] via-[#2b2a2a] to-[#474747]">
+    <div className=" cursor-pointer flex flex-col items-center rounded-2xl h-64 md:h-65 w-[99%]  bg-gradient-to-tr from-[#131313] via-[#2b2a2a] to-[#474747]">
       <h3 className="text-2xl font-bold m-3">Type of Genre</h3>
       <img src="./assets/image.jpg" alt="" className="w-[90%] rounded-2xl " />
     </div>
