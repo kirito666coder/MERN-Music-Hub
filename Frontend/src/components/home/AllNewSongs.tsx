7c7dc2c3283ee6983476c8c913605ca6ff475024
kirito <kirito666codr@gmail.com>
2025-06-19 23:22:50 +0530

    feat: add header to AllNewSongs component

--- Frontend/src/components/home/AllNewSongs.tsx
+++ Frontend/src/components/home/AllNewSongs.tsx
@@ -2,10 +2,13 @@ import AllNewSongCard from "./Components/AllNewSongCard"
 
 const AllNewSongs = () => {
   return (
+    <>
+    <h2 className="mx-6 my-2 text-2xl font-bold">All NewSongs</h2>
     <div className="mx-4 my-2 grid md:grid-cols-2 gap-2">
       <AllNewSongCard/>
       <AllNewSongCard/>
     </div>
+    </>
   )
 }
 
