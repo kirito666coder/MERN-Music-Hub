c7e48f53b3a484bb51c9fc539b9a7a846a452a22
kirito <kirito666codr@gmail.com>
2025-06-21 00:28:01 +0530

    feat: enhance ProfilePageHeader layout with styling and structure

--- Frontend/src/components/Page/ProfilePage/ProfilePageHeader.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfilePageHeader.tsx
@@ -2,8 +2,15 @@
 
 const ProfilePageHeader = () => {
   return (
-    <div>
-      
+    <div className="border-b-2 md:border-b-0 md:border-r-2 pb-5">
+    <div className=" h-40 w-[90%] mx-auto flex items-center gap-3">
+      <div className=" h-35 w-35 rounded-full bg-white "></div>
+       <div className=" ">
+       <h2 className="text-2xl font-bold">User name</h2>
+        <h3 className="text-sm font-semibold">Email Address</h3>
+       </div>
+    </div>
+
     </div>
   )
 }
