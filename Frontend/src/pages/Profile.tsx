46a544ccabf26f1dbb0cdd32bd0f76f4a5527a63
kirito <kirito666codr@gmail.com>
2025-06-21 00:35:23 +0530

    feat: restructure Profile component layout for improved responsiveness

--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -1,11 +1,13 @@
 import ProfilePageHeader from "@/components/Page/ProfilePage/ProfilePageHeader"
 
 const Profile = () => {
-  return (
-    <div>
-      <ProfilePageHeader/>
-    </div>
-  )
+    return (
+        <div className=" flex-row md:flex-col">
+            <div className="md:w-[25%]">
+                <ProfilePageHeader />
+            </div>
+        </div>
+    )
 }
 
 export default Profile
