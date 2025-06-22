e597477d17932a3f5a63bbd0433ff523d87df3e1
kirito <kirito666codr@gmail.com>
2025-06-22 22:11:41 +0530

    feat: enhance ProfilePageHeader to display user information and profile image

--- Frontend/src/components/Page/ProfilePage/ProfilePageHeader.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfilePageHeader.tsx
@@ -1,17 +1,29 @@
-
+import type { RootState } from "@/app/store"
+import { useSelector } from "react-redux"
 
 const ProfilePageHeader = () => {
+  const { user } = useSelector((state: RootState) => state.user)
+ 
+  const highResImage = user?.image?.replace('=s96-c', '=s400-c');
+
   return (
     <div className="border-b-2 md:border-b-0 md:border-r-2 pb-5 md:mt-[37%]">
-    <div className=" h-40 md:h-75 w-[90%] mx-auto flex items-center md:flex-col gap-3">
-      <div className=" h-35 w-35 rounded-full bg-white md:h-60 md:w-60"></div>
-       <div className="md:flex-col md:h-10 md:w-full">
-       <h2 className="text-2xl font-bold">User name</h2>
-        <h3 className="text-sm font-semibold">Email Address</h3>
-       </div>
-    </div>
-    <p className=" h-10 w-[90%] rounded-2xl border-2 mx-auto mt-5 flex items-center px-3 py-1">Bio</p>
-     <button className="h-10 w-[90%] rounded-2xl border-3 mx-auto mt-5 flex items-center justify-center py-5 dark:hover:bg-gray-700 dark:bg-gray-800 cursor-pointer bg-gray-300 hover:bg-gray-200">Edit Profile</button>
+      <div className=" h-40 md:h-75 w-[90%] mx-auto flex items-center md:flex-col gap-3">
+        <div className="h-35 w-35 md:h-60 md:w-60 rounded-full bg-white overflow-hidden">
+          <img
+            src={highResImage}
+            alt="User"
+            className="h-full w-full object-cover filter-none dark:invert-0"
+          />
+        </div>
+
+        <div className="md:flex-col md:h-10 md:w-full">
+          <h2 className="text-2xl font-bold">{user?.name}</h2>
+          <h3 className="text-sm font-semibold">{user?.email}</h3>
+        </div>
+      </div>
+      <p className=" h-10 w-[90%] rounded-2xl border-2 mx-auto mt-5 flex items-center px-3 py-1">Bio</p>
+      <button className="h-10 w-[90%] rounded-2xl border-3 mx-auto mt-5 flex items-center justify-center py-5 dark:hover:bg-gray-700 dark:bg-gray-800 cursor-pointer bg-gray-300 hover:bg-gray-200">Edit Profile</button>
     </div>
   )
 }
