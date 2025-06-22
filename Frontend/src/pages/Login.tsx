1d5a56ce01cee23d36b6367db5efdaaa77fc0895
kirito <kirito666codr@gmail.com>
2025-06-22 22:56:39 +0530

    refactor: remove unused navigation logic and button from Login component

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,23 +1,9 @@
 import GoogleLoginButton from "@/components/Page/LogInpageComponents/GoogleLoginButton"
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-   const navigate = useNavigate();
-
-  const handleClick = () => {
-    console.log("home");
-    navigate("/"); // ✅ Correct programmatic navigation
-  };
   return (
     <div>
       <GoogleLoginButton/>
-      <button
-      onClick={()=>(
-       handleClick()
-  )}
-      >
-        home
-      </button>
     </div>
   )
 }
