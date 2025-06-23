987e3def5b44474a31d889266159843bfbe2bfe4
kirito <kirito666codr@gmail.com>
2025-06-24 00:00:37 +0530

    feat: add navigation links to ProfileNavBar component

--- Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
@@ -4,7 +4,8 @@ const ProfileNavBar = () => {
   return (
     <div className="w-full md:w-[60%] ">
       <ul className="flex gap-4 justify-around items-center text-lg font-semibold">
-       
+        <ProfilePageNavBarLinks to={'/profile'} label={"profile"}/>
+        <ProfilePageNavBarLinks to={'/profile/addSongs'} label={"Add Songs"}/>
       </ul>
     </div>
   )
