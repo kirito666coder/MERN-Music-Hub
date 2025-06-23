e9ae432b5a73c4d3c433f31627e3729a0d1cba45
kirito <kirito666codr@gmail.com>
2025-06-24 01:35:37 +0530

    feat: update ProfileSvgForProfileNavBar to use unique gradient IDs and enhance SVG styling

--- Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
@@ -5,7 +5,7 @@ import ProfileSvgForProfileNavBar from "./ProfileNavBarSvgForLInks/ProfileSvgFor
 const ProfileNavBar = () => {
   return (
     <div className="w-full md:w-[60%] mb-5 border-b-2 md:border-b-0 md:mb-0">
-      <ul className="flex gap-4 justify-around items-center text-lg font-semibold pb-2 ">
+      <ul className="flex flex-wrap  gap-4 justify-around items-center text-lg font-semibold pb-2 ">
         <ProfilePageNavBarLinks to={'/profile'} label={"profile"} icon={ (isActive)=><ProfileSvgForProfileNavBar active={isActive} />}/>
         <ProfilePageNavBarLinks to={'/profile/addSongs'} label={"Add Songs"} icon={ (isActive)=><ProfileSvgForProfileNavBar active={isActive} />}/>
       </ul>
--- Frontend/src/components/Page/ProfilePage/ProfileNavBarSvgForLInks/ProfileSvgForProfileNavBar.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfileNavBarSvgForLInks/ProfileSvgForProfileNavBar.tsx
@@ -1,14 +1,17 @@
+import { useId } from "react";
 
 const ProfileSvgForProfileNavBar = ({ active }: { active?: boolean }) => {
-  const gradientId = "profileGradient" + (active ? "-active" : "-inactive");
+  const uniqueId = useId(); // ensure gradient ID is unique
+  const gradientId = `profileGradient-${uniqueId}`;
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="w-9 h-9"
+      strokeWidth="2.5"
+      stroke={`url(#${gradientId})`}
+      className="w-7 h-7 md:w-9 md:h-9"
     >
       <defs>
         <linearGradient id={gradientId} x1="0" y1="0" x2="1" y2="1">
@@ -17,14 +20,14 @@ const ProfileSvgForProfileNavBar = ({ active }: { active?: boolean }) => {
         </linearGradient>
       </defs>
       <path
-        stroke={`url(#${gradientId})`}
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.25a8.25 8.25 0 0115 0"
       />
     </svg>
   );
-}
+};
 
-export default ProfileSvgForProfileNavBar
+export default ProfileSvgForProfileNavBar;
+
 
