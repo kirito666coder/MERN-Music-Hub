852ed0618f672bb4ca6684b1de6c5a625037c59a
kirito <kirito666codr@gmail.com>
2025-06-23 23:46:01 +0530

    feat: update ProfileNavBar styling and structure for better responsiveness

--- Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfileNavBar.tsx
@@ -1,7 +1,7 @@
 
 const ProfileNavBar = () => {
   return (
-    <div className="w-[60%]">
+    <div className="w-full md:w-[60%] bg-red-500">
       Profile Navbar
     </div>
   )
--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -4,7 +4,9 @@ import ProfilePageHeader from "@/components/Page/ProfilePage/ProfilePageHeader"
 const Profile = () => {
     return (
         <div className=" flex-row md:flex-col">
+            <div className=" flex md:hidden">
             <ProfileNavBar/>
+            </div>
             <div className="md:w-[25%]">
                 <ProfilePageHeader />
             </div>
