ea0e033943881b452e6ff7ec969c9af22d97651e
kirito <kirito666codr@gmail.com>
2025-06-23 23:42:38 +0530

    feat: add ProfileNavBar component to Profile page layout

--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -1,8 +1,10 @@
+import ProfileNavBar from "@/components/Page/ProfilePage/ProfileNavBar"
 import ProfilePageHeader from "@/components/Page/ProfilePage/ProfilePageHeader"
 
 const Profile = () => {
     return (
         <div className=" flex-row md:flex-col">
+            <ProfileNavBar/>
             <div className="md:w-[25%]">
                 <ProfilePageHeader />
             </div>
