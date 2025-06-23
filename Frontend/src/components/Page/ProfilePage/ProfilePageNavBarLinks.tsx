fa3b7670039f9d486f95195d87227a4c7b815a3e
kirito <kirito666codr@gmail.com>
2025-06-24 00:09:07 +0530

    feat: enhance ProfilePageNavBarLinks component with active link styling and TypeScript props

--- Frontend/src/components/Page/ProfilePage/ProfilePageNavBarLinks.tsx
+++ Frontend/src/components/Page/ProfilePage/ProfilePageNavBarLinks.tsx
@@ -1,9 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
-const ProfilePageNavBarLinks = ({to,label,icon}) => {
+interface ProfileNavbarProps  {
+    to: string,
+    label: string,
+    icon: (isActive: boolean) => React.ReactNode
+}
+
+const ProfilePageNavBarLinks = ({to,label,icon}:ProfileNavbarProps) => {
+    const location = useLocation()
+
+    const isActive = location.pathname == to
   return (
     <li>
-        <Link to={to}>
+        <Link to={to} className={` ${isActive ? "text-rose-200" : ""} 
+    font-medium md:ml-2`}>
         <span>{icon}</span>
         <span>{label}</span>
         </Link>
