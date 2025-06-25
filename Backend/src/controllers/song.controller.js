6aa8402f278c9f4128b09c6b4e69c2fa3a17a4b9
kirito <kirito666codr@gmail.com>
2025-06-26 00:18:00 +0530

    feat: implement AddSongController to handle song and file submissions

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,7 +1,11 @@
 
 
 
+// src/controllers/song.controller.js
+export const AddSongController = (req, res) => {
+  console.log("📝 Form Fields:", req.body);
+  console.log("🎧 Audio File:", req.files?.["audioFile"]?.[0]);
+  console.log("🖼️ Image File:", req.files?.["imageFile"]?.[0]);
 
-export const AddSongController = (req,res)=>{
-    console.log(req.body)
-}
+  res.status(200).json({ message: "File and form received!" });
+};
--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { AddSongController } from "../controllers/song.controller.js";
-
+import multer from 'multer'
 
 const SongRouter = Router()
 
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage,
+})
+
 SongRouter.post('/addSong',
+    upload.fields([
+        { name: "audioFile", maxCount: 1 },
+        { name: "imageFile", maxCount: 1 },
+
+    ]),
     AddSongController)
 
 export default SongRouter;
