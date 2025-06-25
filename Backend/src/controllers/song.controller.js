207816fb842bbb5530482973d17492fe98a66a31
kirito <kirito666codr@gmail.com>
2025-06-25 19:08:11 +0530

    feat: implement AddSongController and integrate with song route

---
+++ Backend/src/controllers/song.controller.js
@@ -0,0 +1,7 @@
+
+
+
+
+export const AddSongController = (req,res)=>{
+    
+}
--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { AddSongController } from "../controllers/song.controller.js";
 
 const SongRouter = Router()
 
-
+SongRouter.post('/addSong',AddSongController)
 
 export default SongRouter;
