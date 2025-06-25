89bf760b0dc2aac3eb54a651bf22fdca554061fc
kirito <kirito666codr@gmail.com>
2025-06-25 19:05:51 +0530

    feat: add song router to all routes for API structure

--- Backend/src/routes/all.route.js
+++ Backend/src/routes/all.route.js
@@ -1,9 +1,11 @@
 
 import { Router } from "express";
 import AuthRoute from "./auth.route.js";
+import SongRouter from "./song.route.js";
 
 const AllRoutes = Router()
 
 AllRoutes.use('/auth',AuthRoute)
+AllRoutes.use('/song',SongRouter)
 
 export default AllRoutes;
