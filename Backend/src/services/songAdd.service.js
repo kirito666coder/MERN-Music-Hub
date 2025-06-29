87183a19b16ea0463043913ea552de703fc1611b
kirito <kirito666codr@gmail.com>
2025-06-29 23:00:55 +0530

    fix: validate userId and required fields in AddSong function

--- Backend/src/services/songAdd.service.js
+++ Backend/src/services/songAdd.service.js
@@ -1,6 +1,40 @@
+import UserModel from "../Models/user.model"
 
+export const AddSong = async ({ userId, data, audioUrl, imageUrl }) => {
 
+    if (!userId || !data || !audioUrl || !imageUrl) {
+        throw new Error("some fields are mesing")
+    }
 
-export const AddSong = ()=>{
-    
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new Error("Invalid user ID");
+    }
+
+    const user = await UserModel.findById({ _id: userId })
+
+    if (!user) {
+        throw new Error("user not found")
+    }
+
+  const {
+    title,
+    artist,
+    duration,
+    album,
+    genre,
+    language,
+    lyrics,
+    description,
+    releaseDate,
+    tags,
+    isPublic,
+    visibility,
+    mood,
+  } = data;
+
+    if (!title || !artist || !duration) {
+    throw new Error("Title, artist, and duration are required");
+  }
+
+  
 }
