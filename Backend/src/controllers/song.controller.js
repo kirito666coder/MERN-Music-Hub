199f809bcaf2f22e53f971ea56a59d3c00c97d2f
kirito <kirito666codr@gmail.com>
2025-06-29 19:41:52 +0530

    refactor: move bufferToDataURL function to cloudinaryUpload service

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,10 +1,3 @@
-import cloudinary from "../configs/cloudinary/cloudinary.config.js";
-
-const bufferToDataURL = (fileBuffer,mimetype) =>{
-  const base64 = fileBuffer.toString('base64');
-  return `data:${mimetype};base64,${base64}`
-}
-
 
 export const AddSongController = async (req, res) => {
      try {
