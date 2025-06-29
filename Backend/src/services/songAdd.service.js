d542d7330368ec26824e45a769bc91de00c7b766
kirito <kirito666codr@gmail.com>
2025-06-29 23:06:46 +0530

    fix: correct error handling and formatting in AddSong function

--- Backend/src/services/songAdd.service.js
+++ Backend/src/services/songAdd.service.js
@@ -1,3 +1,4 @@
+import SongModel from "../Models/song.model";
 import UserModel from "../Models/user.model"
 
 export const AddSong = async ({ userId, data, audioUrl, imageUrl }) => {
@@ -16,25 +17,44 @@ export const AddSong = async ({ userId, data, audioUrl, imageUrl }) => {
         throw new Error("user not found")
     }
 
-  const {
-    title,
-    artist,
-    duration,
-    album,
-    genre,
-    language,
-    lyrics,
-    description,
-    releaseDate,
-    tags,
-    isPublic,
-    visibility,
-    mood,
-  } = data;
+    const {
+        title,
+        artist,
+        duration,
+        album,
+        genre,
+        language,
+        lyrics,
+        description,
+        releaseDate,
+        tags,
+        isPublic,
+        visibility,
+        mood,
+    } = data;
 
     if (!title || !artist || !duration) {
-    throw new Error("Title, artist, and duration are required");
-  }
+        throw new Error("Title, artist, and duration are required");
+    }
 
-  
+    const newSong = await SongModel.create({
+        userId,
+        title,
+        artist,
+        duration,
+        album: album || 'Single',
+        genre: genre || [],
+        language: language || "Unknown",
+        lyrics: lyrics || '',
+        description: description || "",
+        releaseDate: releaseDate || new Date(),
+        audioUrl,
+        coverUrl: imageUrl,
+        tags: tags || [],
+        isPublic: isPublic != undefined ? isPublic : true,
+        visibility: visibility || "public",
+        mood: mood || 'none',
+    })
+
+    return newSong;
 }
