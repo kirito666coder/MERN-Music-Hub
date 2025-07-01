68d10083d51d04c452581220edaaac4a8680ab67
kirito <kirito666codr@gmail.com>
2025-07-01 16:23:02 +0530

    refactor: move SongFormData interface to song.ts for better organization

---
+++ Frontend/src/types/song.ts
@@ -0,0 +1,19 @@
+
+
+export interface SongFormData {
+  _id:string;
+  title: string;
+  artist: string;
+  album: string;
+  genre: string[];
+  language: string;
+  releaseDate: string;
+  lyrics: string;
+  description: string;
+  tags: string[];
+  isPublic: boolean;
+  mood: string;
+  duration: number;
+  audioFile: File | null;
+  imageFile: File | null;
+}
--- Frontend/src/types/user.ts
+++ Frontend/src/types/user.ts
@@ -9,21 +9,3 @@ export type User = {
 };
 
 
-
-export interface SongFormData {
-  _id:string;
-  title: string;
-  artist: string;
-  album: string;
-  genre: string[];
-  language: string;
-  releaseDate: string;
-  lyrics: string;
-  description: string;
-  tags: string[];
-  isPublic: boolean;
-  mood: string;
-  duration: number;
-  audioFile: File | null;
-  imageFile: File | null;
-}
