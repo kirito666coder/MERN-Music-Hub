80acf75c2f93e4b09dbca07184fe5a5a7b2255e0
kirito <kirito666codr@gmail.com>
2025-07-04 23:34:22 +0530

    feat: add SongPlayBarSongLogoAndTital component to SongPlayBar

--- Frontend/src/components/MainLayoutComponents/SongPlayBar.tsx
+++ Frontend/src/components/MainLayoutComponents/SongPlayBar.tsx
@@ -1,8 +1,9 @@
+import SongPlayBarSongLogoAndTital from "./SongPlayBarComponents/SongPlayBarSongLogoAndTital"
 
 const SongPlayBar = () => {
   return (
     <div className="h-15 w-full bg-white">
-      
+      <SongPlayBarSongLogoAndTital/>
     </div>
   )
 }
