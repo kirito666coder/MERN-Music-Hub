817babafab6ec4b121d3099c9fe99b7a4657d432
kirito <kirito666codr@gmail.com>
2025-07-05 00:16:28 +0530

    fix: update layout of SongPlayBar and SongPlayBarSongLogoAndTital components for better responsiveness

--- Frontend/src/components/MainLayoutComponents/SongPlayBar.tsx
+++ Frontend/src/components/MainLayoutComponents/SongPlayBar.tsx
@@ -2,14 +2,14 @@ import SongPlayBarSongLogoAndTital from "./SongPlayBarComponents/SongPlayBarSong
 
 const SongPlayBar = () => {
   return (
-    <div className="h-19 w-full bg-white flex justify-center items-center">
-      <div className="w-1/3">
+    <div className="h-19 w-full bg-black flex justify-center items-center">
+      <div className="w-4/10 md:w-3/10">
       <SongPlayBarSongLogoAndTital/>
       </div>
-      <div className="w-1/3">
+      <div className="w-3/10 md:w-4/10">
  
       </div>
-      <div className="w-1/3">
+      <div className="w-3/10">
   
       </div>
     </div>
--- Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
+++ Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
@@ -1,19 +1,19 @@
 
 const SongPlayBarSongLogoAndTital = () => {
-  return (
-    <div className="flex ml-2 ">
-      <div className="bg-red-600 h-15 min-w-15 rounded-2xl overflow-hidden">
-        <img src="./assets/image.jpg" alt="" className="h-full w-full object-cover" />
-      </div>
-     <div className="w-full overflow-hidden">
-  <h3 className="bg-amber-700 w-full text-2xl font-bold overflow-hidden text-ellipsis whitespace-nowrap">
-    Tjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjital
-  </h3>
-  <h3 className="bg-amber-700 w-full text-lg font-semibold overflow-hidden text-ellipsis whitespace-nowrap">Tital</h3>
-</div>
+    return (
+        <div className="flex ml-2 ">
+            <div className=" h-15 min-w-15 rounded-2xl overflow-hidden">
+                <img src="./assets/image.jpg" alt="" className="h-full w-full object-cover" />
+            </div>
+            <div className="w-full overflow-hidden ml-3">
+                <h3 className=" w-full text-lg font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
+                    Tjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjital
+                </h3>
+                <h3 className=" w-full font-medium overflow-hidden text-ellipsis whitespace-nowrap">Tital</h3>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default SongPlayBarSongLogoAndTital
