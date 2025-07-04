52ca4c582e8be0a5e6a1e832c8514c4f3cc179e6
kirito <kirito666codr@gmail.com>
2025-07-05 00:05:42 +0530

    feat: enhance layout of SongPlayBarSongLogoAndTital component with improved title styling

--- Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
+++ Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
@@ -5,9 +5,13 @@ const SongPlayBarSongLogoAndTital = () => {
       <div className="bg-red-600 h-15 min-w-15 rounded-2xl overflow-hidden">
         <img src="./assets/image.jpg" alt="" className="h-full w-full object-cover" />
       </div>
-      <div className="w-full">
-        <h3 className="bg-amber-700 h-15 w-full">Tital</h3>
-      </div>
+     <div className="w-full overflow-hidden">
+  <h3 className="bg-amber-700 w-full text-2xl font-bold overflow-hidden text-ellipsis whitespace-nowrap">
+    Tjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjital
+  </h3>
+  <h3 className="bg-amber-700 w-full text-lg font-semibold overflow-hidden text-ellipsis whitespace-nowrap">Tital</h3>
+</div>
+
     </div>
   )
 }
