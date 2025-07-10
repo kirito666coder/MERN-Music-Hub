cf27cd68cf64035a12bf795b3d3d9549b2185c94
kirito <kirito666codr@gmail.com>
2025-07-10 18:56:56 +0530

    feat: update SongPlayBarSongLogoAndTital to use dynamic cover image from props for improved song display

--- Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
+++ Frontend/src/components/MainLayoutComponents/SongPlayBarComponents/SongPlayBarSongLogoAndTital.tsx
@@ -8,7 +8,7 @@ const SongPlayBarSongLogoAndTital = () => {
     return (
         <div className="flex ml-2 ">
             <div className=" h-19 md:h-15 min-w-15 rounded-2xl overflow-hidden">
-                <img src="./assets/image.jpg" alt="" className="h-full w-full object-cover" />
+                <img src={`${coverImage}`} alt="" className="h-full w-full object-cover" />
             </div>
             <div className="w-full overflow-hidden md:block ml-3 hidden">
                 <h3 className=" w-full text-lg font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
