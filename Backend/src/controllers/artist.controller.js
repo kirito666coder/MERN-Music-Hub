b14c60453623bfe5a0d4d34905cb43210e3db6c1
kirito <kirito666codr@gmail.com>
2025-07-14 23:03:03 +0530

    feat: enhance SearchArtistController to handle search queries and return artist data with error handling

--- Backend/src/controllers/artist.controller.js
+++ Backend/src/controllers/artist.controller.js
@@ -1,5 +1,24 @@
+import ArtistModel from "../Models/artist.model.js";
 
 
-export const SearchArtistController = (req,res) =>{
+export const SearchArtistController = async (req,res) =>{
+    try {
+        const search = req.query.search;
+        
+        if(!search || search.trim() === ''){
+            return res.status(400).json({message:"missing search query"});
+        }
 
+        const artists = await ArtistModel.find({
+            name:{$regex:search,$options:'i'}
+        })
+        .limit(10)
+        .select('name _id photoUrl');
+        
+        res.json({artists})
+
+    } catch (error) {
+        console.error("Error searching artists:", error);
+        res.status(500).json({ message: "Server error" });
+    }
 }
