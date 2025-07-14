8b2be99543f18adae0b2b08da29f46fdef7014b4
kirito <kirito666codr@gmail.com>
2025-07-14 22:26:42 +0530

    feat: add Album model with schema definition for title, artistId, coverUrl, description, releaseDate, genres, and songs

---
+++ Backend/src/Models/album.model.js
@@ -0,0 +1,41 @@
+import { Schema } from "mongoose";
+
+
+const AlbumSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    artistId:{
+        type:Schema.Types.ObjectId,
+        ref:"Artist",
+        required:true,
+    },
+    coverUrl:{
+        type:String,
+        default:'',
+    },
+    description:{
+        type:String,
+        default:'',
+    },
+    releaseDate:{
+        type:Date,
+        default:Date.now,
+    },
+    genres:{
+        type:[String],
+        default:[],
+    },
+    songs:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Songs',
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+});
