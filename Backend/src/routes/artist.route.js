dd9eda0e3fd4e8e632fc76a6dc7a2d58b602d4dd
kirito <kirito666codr@gmail.com>
2025-07-14 22:43:37 +0530

    feat: add search route for artist in artist.route.js

--- Backend/src/routes/artist.route.js
+++ Backend/src/routes/artist.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { SearchArtistController } from "../controllers/artist.controller.js";
 
 const ArtistRouder = Router()
 
-
+ArtistRouder.get('/search',SearchArtistController)
 
 export default ArtistRouder;
