de3dbf5a74f48c279e0ce0b2b7e0842f887ecbaa
kirito <kirito666codr@gmail.com>
2025-07-19 23:43:03 +0530

    feat: add create album controller and route for album creation

--- Backend/src/routes/album.route.js
+++ Backend/src/routes/album.route.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import { createAlbumController } from '../controllers/album.controller.js';
 
 const AlbumRouter = Router();
 
-
+AlbumRouter.post('/create',createAlbumController)
 
 export default AlbumRouter;
