b6bedb7a37cfcce875327d377d467ff813489710
kirito <kirito666codr@gmail.com>
2025-07-21 22:51:02 +0530

    feat: update createAlbumController and CreateAlbumService to include artist ID validation and error handling

--- Backend/src/controllers/album.controller.js
+++ Backend/src/controllers/album.controller.js
@@ -1,8 +1,9 @@
 import { CreateAlbumService } from "../services/album.service.js";
+import { FindArtistService } from "../services/artist.service.js";
 
 
 
-export const createAlbumController = (req, res) => {
+export const createAlbumController = async (req, res) => {
     try {
         const data = req.body;
 
@@ -12,12 +13,12 @@ export const createAlbumController = (req, res) => {
 
         const user = req.user;
 
-        
+        const artistId = await FindArtistService({userId:user._id})
 
 
-        const album = CreateAlbumService({data,})
+        const album = await CreateAlbumService({data,artistId})
 
-
+        res.status(200).json({album})
 
     } catch (error) {
     res.status(500).json({message:"Internal server error",error})
--- Backend/src/services/album.service.js
+++ Backend/src/services/album.service.js
@@ -1,5 +1,21 @@
 
 
-export const CreateAlbumService = () =>{
-    
+export const CreateAlbumService = async ({data,artistId}) =>{
+
+    if (!artistId) {
+        throw new Error("ArtistID is missing")
+    }
+    if (!data) {
+        throw new Error("data is missing")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new Error("Invalid user ID");
+    }
+
+    try {
+        
+    } catch (error) {
+        throw new Error ('Failled to create album ')
+    }
 }
