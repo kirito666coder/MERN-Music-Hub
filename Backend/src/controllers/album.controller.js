7ce0d0e89a0ca522888eb7c2dbfa87c0634f2c42
kirito <kirito666codr@gmail.com>
2025-07-21 22:31:25 +0530

    feat: implement createAlbumController with request validation and error handling

--- Backend/src/controllers/album.controller.js
+++ Backend/src/controllers/album.controller.js
@@ -1,4 +1,20 @@
 
-export const createAlbumController =()=>{
 
+
+export const createAlbumController = (req, res) => {
+    try {
+        const data = req.body;
+
+        if(!data){
+            return res.status(400).json({message:"data is Empty! Please try again"})
+        }
+
+
+        const album = 
+
+
+
+    } catch (error) {
+    res.status(500).json({message:"Internal server error",error})
+    }
 }
