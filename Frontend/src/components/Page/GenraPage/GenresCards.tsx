404cd53af4c9a96feb03b10ce5eb51a93d88798a
kirito <kirito666codr@gmail.com>
2025-07-27 23:34:53 +0530

    refactor: update GenresCards component to GenresCard, enhancing genre display with dynamic name and image properties

--- Frontend/src/components/Page/GenraPage/GenresCards.tsx
+++ Frontend/src/components/Page/GenraPage/GenresCards.tsx
@@ -1,11 +1,16 @@
+// components/GenresCard.tsx
+type Genre = {
+  name: string;
+  image: string;
+};
 
-const GenresCards = () => {
+const GenresCard = ({ genre }: { genre: Genre }) => {
   return (
-    <div className=" cursor-pointer flex flex-col items-center rounded-2xl h-64 md:h-65 w-[99%]  bg-gradient-to-tr from-[#131313] via-[#2b2a2a] to-[#474747]">
-      <h3 className="text-2xl font-bold m-3">Type of Genre</h3>
-      <img src="./assets/image.jpg" alt="" className="w-[90%] rounded-2xl " />
+    <div className="cursor-pointer flex flex-col items-center rounded-2xl h-64 md:h-65 w-[99%] bg-gradient-to-tr from-[#131313] via-[#2b2a2a] to-[#474747]">
+      <h3 className="text-2xl font-bold m-3">{genre?.name}</h3>
+      <img src={genre?.image} alt={genre?.name} className="w-[90%] rounded-2xl" />
     </div>
-  )
-}
+  );
+};
 
-export default GenresCards
+export default GenresCard;
