49107fad67cf9640e627763c248b96a4726047bd
kirito <kirito666codr@gmail.com>
2025-07-27 23:35:40 +0530

    feat: add MoodCard component to display mood name and image with styled layout

---
+++ Frontend/src/components/Page/GenraPage/MoodCard.tsx
@@ -0,0 +1,16 @@
+type Mood = {
+    name: string;
+    image: string;
+  };
+  
+  const MoodCard = ({ mood }: { mood: Mood }) => {
+    return (
+      <div className="cursor-pointer flex flex-col items-center rounded-2xl h-64 md:h-65 w-[99%] bg-gradient-to-tr from-[#131313] via-[#2b2a2a] to-[#474747]">
+        <h3 className="text-2xl font-bold m-3">{mood.name}</h3>
+        <img src={mood.image} alt={mood.name} className="w-[90%] rounded-2xl" />
+      </div>
+    );
+  };
+  
+  export default MoodCard;
+  
