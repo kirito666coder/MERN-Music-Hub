e171275e827e822dbe9ccb35036937d493c82acb
kirito <kirito666codr@gmail.com>
2025-08-01 10:30:02 +0530

    feat: implement playSongByIndex function to play a song from the queue by its index and manage playback state

---
+++ Frontend/src/utils/PlaySongByIndex.ts
@@ -0,0 +1,32 @@
+import { GetSong } from "@/api/SongApi";
+import type { AppDispatch } from "@/app/store";
+import { setIsPlaying, setSong, setSongDetails } from "@/features/song/songSlice";
+import type { SongData } from "@/types/song";
+
+
+
+export const playSongByIndex = async (
+    queue:SongData[],
+    index:number,
+    dispatch:AppDispatch
+)=>{
+    const song = queue[index];
+    if(!song)return;
+
+    try {
+        const audioUrl = await GetSong({songId:song._id})
+        if(audioUrl?.songurl){
+            dispatch(setSong(audioUrl.songurl))
+            dispatch(setSongDetails({
+                title: song.title ?? null,
+                artist: song.artist?.name ?? null,
+                coverImage: song.coverUrl ?? null 
+            }))
+            dispatch(setIsPlaying(true));
+        }else{
+            console.log('Song URL not found')
+        }
+    } catch (error) {
+        console.log("Error fetching song:",error)
+    }
+}
