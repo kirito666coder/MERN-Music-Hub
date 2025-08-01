09866027b49c44d978b773b0e0dea818ae590aad
kirito <kirito666codr@gmail.com>
2025-08-01 15:34:06 +0530

    refactor: modularize AlbumCompo by introducing AlbumSongPlayCard for song display and interaction

--- Frontend/src/components/Page/AlbumPage/AlbumCompo.tsx
+++ Frontend/src/components/Page/AlbumPage/AlbumCompo.tsx
@@ -1,10 +1,8 @@
-import { FaPlay} from "react-icons/fa";
 import { getAlbumApi } from "@/api/AlbumApi";
-import LikeButton from "@/components/icons/LikeButton";
 import type { Album } from "@/types/album";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import AlbumPlaysongButton from "./AlbumPlaysongButton";
+import AlbumSongPlayCard from "./AlbumSongPlayCard";
 const AlbumCompo = () => {
   
     const [album, setalbum] = useState<Album|null>(null)
@@ -89,37 +87,7 @@ const AlbumCompo = () => {
   
   
             <ul className="space-y-2 ">
-    {album?.songs.map((song, idx) => (
-      <li
-        key={idx}
-        className="flex items-center gap-4 p-2 rounded  transition border-2 "
-      >
-        {/* Cover thumbnail */}
-        <img
-          src={song.coverUrl}
-          alt={song.title}
-          className="w-12 h-12 rounded object-cover shadow"
-        />
-  
-        {/* Song info */}
-        <div className="flex-1">
-          <div className="font-semibold truncate text-lg">{song.title}</div>
-          <div className="flex gap-3 text-xs text-gray-500">
-            <span className="flex justify-center items-center gap-0.5 text-[17px] text-white"><LikeButton/> {song.likes}</span>
-            <span className="flex justify-center items-center text-[17px] text-white gap-1.5"> <FaPlay/>{song.plays}</span>
-          </div>
-        </div>
-  
-        {/* Optional: play button */}
-        <div
-         onClick={()=>{
-          console.log('click outside album playsong button')
-         }}
-         className="p-2 rounded-full hover:bg-gray-200 hover:scale-105 transition">
-         <AlbumPlaysongButton/>
-        </div>
-      </li>
-    ))}
+            <AlbumSongPlayCard album={album}/>
   </ul>
   
           </div>
--- Frontend/src/components/Page/AlbumPage/AlbumSongPlayCard.tsx
+++ Frontend/src/components/Page/AlbumPage/AlbumSongPlayCard.tsx
@@ -1,9 +1,43 @@
+import type { Album } from "@/types/album"
+import { FaPlay} from "react-icons/fa";
+import LikeButton from "@/components/icons/LikeButton";
+import AlbumPlaysongButton from "./AlbumPlaysongButton";
 
-const AlbumSongPlayCard = () => {
+const AlbumSongPlayCard = ({album}:{album:Album}) => {
   return (
-    <div>
-      
-    </div>
+    <>
+       {album?.songs.map((song, idx) => (
+      <li
+        key={idx}
+        className="flex items-center gap-4 p-2 rounded  transition border-2 "
+      >
+        {/* Cover thumbnail */}
+        <img
+          src={song.coverUrl}
+          alt={song.title}
+          className="w-12 h-12 rounded object-cover shadow"
+        />
+  
+        {/* Song info */}
+        <div className="flex-1">
+          <div className="font-semibold truncate text-lg">{song.title}</div>
+          <div className="flex gap-3 text-xs text-gray-500">
+            <span className="flex justify-center items-center gap-0.5 text-[17px] text-white"><LikeButton/> {song.likes}</span>
+            <span className="flex justify-center items-center text-[17px] text-white gap-1.5"> <FaPlay/>{song.plays}</span>
+          </div>
+        </div>
+  
+        {/* Optional: play button */}
+        <div
+         onClick={()=>{
+          console.log('click outside album playsong button')
+         }}
+         className="p-2 rounded-full hover:bg-gray-200 hover:scale-105 transition">
+         <AlbumPlaysongButton/>
+        </div>
+      </li>
+    ))}
+    </>
   )
 }
 
