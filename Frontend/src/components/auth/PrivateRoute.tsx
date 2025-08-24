088fcdbd8435ccb0c9175db0a7fde139ef51425c
kirito <kirito666codr@gmail.com>
2025-08-24 15:51:51 +0530

    refactor: remove LoadingPageForAuth component and update PrivateRoute and PublicRoute to handle loading state without it

--- Frontend/src/components/auth/PrivateRoute.tsx
+++ Frontend/src/components/auth/PrivateRoute.tsx
@@ -1,12 +1,11 @@
 import { useSelector } from "react-redux"
 import type { RootState } from "../../app/store"
 import { Navigate } from "react-router-dom"
-import LoadingPageForAuth from "../loading/LoadingPageForAuth"
 
 const PrivateRoute = ({children}:{children:JSX.Element}) => {
   const {user,loading} = useSelector((state:RootState)=>state.user)
 
-   if(loading) return <LoadingPageForAuth/>
+   if(loading) return 
 
   return user ? children : <Navigate to="/login"/>
   
