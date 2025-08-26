512df228cb2fb8f86d631ab015fdfe39858496ec
kirito <kirito666codr@gmail.com>
2025-08-26 18:34:44 +0530

    feat: implement song and artist search functionality in SearchPage using API integration

--- Frontend/src/pages/SearchPage.tsx
+++ Frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,31 @@
+import { SearchSongsApi } from "@/api/SongApi";
+import type { Artist } from "@/types/artist";
+import type { SongData } from "@/types/song";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 
 const SearchPage = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
+    const { slug } = useParams()
+
+    const [songs, setSongs] = useState<SongData[]>([]);
+    const [artists, setArtists] = useState<Artist[]>([]);
+    const handleSearch = async () => {
+        if (!slug) return;
+        const data = await SearchSongsApi(slug)
+        setSongs(data?.songs || []);
+        setArtists(data?.artists || []);
+    };
+
+    useEffect(() => {
+        handleSearch()
+    }, [])
+
+
+    return (
+        <div className="h-full w-full flex justify-center items-center text-5xl">
+            {slug}
+        </div>
+    )
 }
 
 export default SearchPage
